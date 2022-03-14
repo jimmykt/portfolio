df1572f1ce4fe1fc66cd6a8eb7cda41331cf73ae
Ka-Kit Jimmy Tam <90652743+jimmykt@users.noreply.github.com>
2022-03-14 14:02:34 -0400

    test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,20 @@ import "./Header.scss";
 function Header() {
   return (
     <header className="Header">
-      <p>Jimmy Tam</p>
-      <ul className="Header__nav">
-        <li className="Header__link">
+      <p className="Header__name">Jimmy Tam</p>
+
+      <span></span>
+      <span></span>
+      <span></span>
+
+      <ul className="Header__list">
+        <li className="Header__item">
           <a href="#hero">Home</a>
         </li>
-        <li className="Header__link">
+        <li className="Header__item">
           <a href="#projects">Projects</a>
         </li>
-        <li className="Header__link">Contact</li>
+        <li className="Header__item">Contact</li>
       </ul>
     </header>
   );
