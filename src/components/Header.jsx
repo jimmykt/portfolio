e4e439526de9a8b90d793925aa2d0c85c33770da
Ka-Kit Jimmy Tam <90652743+jimmykt@users.noreply.github.com>
2022-03-14 16:59:22 -0400

    hamburger

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,11 @@
 import "./Header.scss";
+import Toggle from "./Toggle";
 
 function Header() {
   return (
     <header className="Header">
-      <p className="Header__name">Jimmy Tam</p>
-
-      <span></span>
-      <span></span>
-      <span></span>
-
-      <ul className="Header__list">
-        <li className="Header__item">
-          <a href="#hero">Home</a>
-        </li>
-        <li className="Header__item">
-          <a href="#projects">Projects</a>
-        </li>
-        <li className="Header__item">Contact</li>
-      </ul>
+      <h1 className="Header__name">Jimmy Tam</h1>
+      <Toggle />
     </header>
   );
 }
