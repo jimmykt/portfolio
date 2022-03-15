bbd63b41c7ea8c0629c1753e5a9b253d4cb736a2
Ka-Kit Jimmy Tam <90652743+jimmykt@users.noreply.github.com>
2022-03-15 16:25:26 -0400

    mobile nav and hero

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,40 @@
 import "./Header.scss";
-import Toggle from "./Toggle";
+import React, { useState } from "react";
+import Hamburger from "./Hamburger";
 
 function Header() {
+  const [isClick, setIsClick] = useState("dont-show-menu");
+
+  const toggle = () => {
+    if (isClick === "dont-show-menu") {
+      setIsClick("show-menu");
+    } else {
+      setIsClick("dont-show-menu");
+    }
+  };
+
   return (
-    <header class="top-nav">
-      <div>Jimmy Tam</div>
-      <input id="menu-toggle" type="checkbox" />
-      <label class="menu-button-container" for="menu-toggle">
-        <div class="menu-button"></div>
-      </label>
-      <ul class="menu">
-        <li>Home</li>
-        <li>Projects</li>
-        <li>Contact</li>
+    <header class="header">
+      <h1 className="header__logo">Jimmy Tam</h1>
+
+      <Hamburger onClick={toggle} />
+
+      <ul className={"header__list " + isClick}>
+        <li className="header__list-item">
+          <a className="header__link" href="#">
+            Home
+          </a>
+        </li>
+        <li className="header__list-item">
+          <a className="header__link" href="#">
+            Projects
+          </a>
+        </li>
+        <li className="header__list-item">
+          <a className="header__link" href="#">
+            Contact
+          </a>
+        </li>
       </ul>
     </header>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,10 +2,10 @@ import "./Hero.scss";
 
 function Hero() {
   return (
-    <div className="Hero" id="hero">
-      <p className="Hero__intro">Hi, My Name is Jimmy</p>
-      <p className="Hero__message">Enjoy some of the projects i've made.</p>
-    </div>
+    <section className="Hero" id="hero">
+      <h1 className="Hero__intro">Hi, My Name is Jimmy,</h1>
+      <h2 className="Hero__message">Enjoy some of the projects i've made.</h2>
+    </section>
   );
 }
 
