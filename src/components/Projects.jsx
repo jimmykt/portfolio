159bfda9c4591e2264e1f68959736022505644b0
jimmykt <jimmykkst@gmail.com>
2022-08-24 14:41:16 -0400

    slider completed

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -38,42 +38,14 @@ function Projects() {
       <div className={"Projects__container"}>
         <ProjectCard
           image={data[displayProject].image}
-          name="Track"
-          info="An expense tracking app, secure login with webtokens, and secure signups with bcryptjs"
-          stacks="React.js, axios, redux, react-router-dom, Node, MongoDB, Mongoose, sass, redux,  Express, bcryptjs, jsonwebtoken "
-          moreInfo="signup, or use a test account:  email:john Password:smith"
-          github="https://github.com/jimmykt/track"
-          host="https://track-app-jt.netlify.app/"
-          verticalImage={false}
+          name={data[displayProject].name}
+          info={data[displayProject].info}
+          stacks={data[displayProject].stacks}
+          moreInfo={data[displayProject].moreInfo}
+          github={data[displayProject].github}
+          host={data[displayProject].host}
+          verticalImage={data[displayProject].verticalImage}
         />
-        {/* <ProjectCard
-          image={TrackImage}
-          name="Track"
-          info="An expense tracking app, secure login with webtokens, and secure signups with bcryptjs"
-          stacks="React.js, axios, redux, react-router-dom, Node, MongoDB, Mongoose, sass, redux,  Express, bcryptjs, jsonwebtoken "
-          moreInfo="signup, or use a test account:  email:john Password:smith"
-          github="https://github.com/jimmykt/track"
-          host="https://track-app-jt.netlify.app/"
-          verticalImage={false} 
-        />*/}
-        {/* <ProjectCard
-            image={JourneyImage}
-            name="Journey"
-            info="A journal application for users to post about an idea, day with an image. Like Counter for users, Delete and update functionality"
-            stacks="React.js, axios, redux, react-file-base64, Node, MongoDB, Express"
-            github="https://github.com/jimmykt/Journey"
-            host="https://journey-mern.netlify.app/"
-            verticalImage={false}
-          />
-          <ProjectCard
-            image={DogPoolImage}
-            name="DogPool"
-            info="Dog pool, an application that allows your loved dog to be walked when you are too busy or sick. Dog pool lets you set up a distance on how far of a walk you are going on. The app finds dogs that need a walk within your walking distance for you to pick up and help out a friendly neighbor. This app allows you to explore new walking paths, lets you dog meet new friends and have your dog walked when you aren’t able to walk him."
-            stacks="React.js, axios, redux, react-file-base64, Node, MySQL, Express, Google API"
-            github="https://github.com/jimmykt/dog-pool"
-            host="https://journey-mern.netlify.app/"
-            verticalImage={true}
-          /> */}
       </div>
       <img
         className="Projects__arrow Projects__arrow--right"
--- src/data.js
+++ src/data.js
@@ -16,14 +16,25 @@ const data = [
   {
     image:
       "https://drive.google.com/uc?export=view&id=1FIHThIZjdzQRZitIrMx_m3gl0biem1wr",
-    name: "Traasdasdack",
-    info: "An expense tracking app, secure login with webtokens, and secure signups with bcryptjs",
-    stacks:
-      "React.js, axios, redux, react-router-dom, Node, MongoDB, Mongoose, sass, redux,  Express, bcryptjs, jsonwebtoken ",
+    name: "Journey",
+    info: "A journal application for users to post about an idea, day with an image. Like Counter for users, Delete and update functionality",
+    stacks: "React.js, axios, redux, react-file-base64, Node, MongoDB, Express",
     moreInfo: "signup, or use a test account:  email:john Password:smith",
-    github: "https://github.com/jimmykt/track",
-    host: "https://track-app-jt.netlify.app/",
+    github: "https://github.com/jimmykt/Journey",
+    host: "https://journey-mern.netlify.app/",
     verticalImage: false,
+  },
+  {
+    image:
+      "https://drive.google.com/uc?export=view&id=1XVZuVSQiuELjCXNU3CxDGh2Xcvkz6rNh",
+    name: "DogPool",
+    info: "Dog pool, an application that allows your loved dog to be walked when you are too busy or sick. Dog pool lets you set up a distance on how far of a walk you are going on. The app finds dogs that need a walk within your walking distance for you to pick up and help out a friendly neighbor. This app allows you to explore new walking paths, lets you dog meet new friends and have your dog walked when you aren’t able to walk him.",
+    stacks:
+      "React.js, axios, redux, react-file-base64, Node, MySQL, Express, Google API",
+    moreInfo: "signup, or use a test account:  email:john Password:smith",
+    github: "https://github.com/jimmykt/dog-pool",
+    host: "https://journey-mern.netlify.app/",
+    verticalImage: true,
   },
 ];
 
